790c1a3082cabb5af0c47a37874e7edec1bdce8f
SimonaPopescuIT <158314180+SimonaPopescuIT@users.noreply.github.com>
2024-06-19 17:39:15 +0300

    Navbar + About + DetaliiAnticelulitic + DetaliiRelaxare + DetaliiTerapeutic + Home + Services

--- src/Pages/DetaliiAnticelulitic.jsx
+++ src/Pages/DetaliiAnticelulitic.jsx
@@ -11,23 +11,24 @@ const DetaliiAnticelulitic = () => {
       <Navbar />
       <div className='container'>
         <div className='text3'>
-          <h2 className='title'>Scapă de Celulită cu Masajul Anticelulitic!</h2>
+          <h1 className='title'>Scapă de Celulită cu Masajul Anticelulitic!</h1>
 
           <p>
             Dacă sunteți în căutarea unei soluții eficiente și plăcute pentru a combate celulita, masajul anticelulitic este exact ceea ce aveți nevoie. Această tehnică specializată ajută la descompunerea depozitelor de grăsime și la îmbunătățirea aspectului pielii, oferindu-vă o piele mai fermă și mai sănătoasă.
 
           </p>
           <h3>Beneficiile Masajului Anticelulitic</h3>
-          <p>1. Reducerea Celulitei:
-            Masajul anticelulitic ajută la descompunerea grăsimilor subcutanate și la uniformizarea aspectului pielii, reducând vizibilitatea celulitei.
+          Reducerea Celulitei:
+          <ol>
+            <li>Masajul anticelulitic ajută la descompunerea grăsimilor subcutanate și la uniformizarea aspectului pielii, reducând vizibilitatea celulitei.</li> 
 
-            2. Îmbunătățirea Aspectului Pielii:
-            Această tehnică stimulează producția de colagen și elastină, esențiale pentru o piele tonifiată și elastică. Pielea dumneavoastră va deveni mai fermă și mai netedă.<br />
+            <li> Îmbunătățirea Aspectului Pielii:
+            Această tehnică stimulează producția de colagen și elastină, esențiale pentru o piele tonifiată și elastică. </li>
 
-            <br />
-            3. Drenaj Limfatic:
+            <li> Drenaj Limfatic:
             Masajul include tehnici de drenaj limfatic, care ajută la eliminarea toxinelor din organism și la îmbunătățirea circulației limfatice. Veți simți o senzație de lejeritate și de bunăstare generală.
-          </p>
+            </li>
+          </ol>
 
           <h3>Concluzie</h3>
           <p>Masajul anticelulitic este o soluție eficientă și plăcută pentru combaterea celulitei. Prin tehnici specializate, acest masaj ajută la reducerea stratului de celulită, îmbunătățește fermitatea și aspectul pielii și stimulează drenajul limfatic. În combinație cu un stil de viață sănătos, masajul anticelulitic poate oferi rezultate remarcabile, contribuind la o piele mai fermă și la un organism detoxifiat.</p>
--- src/Pages/DetaliiRelaxare.jsx
+++ src/Pages/DetaliiRelaxare.jsx
@@ -4,6 +4,7 @@ import Form from '../Components/Parts/Form';
 import "../Pages/Services.css";
 import relax from '../Assets/picior1.jpg';
 import relax1 from '../Assets/pietre.jpg';
+import scoica from '../Assets/scoica.jpg';
 
 const DetaliiRelaxare = () => {
   return (
@@ -11,40 +12,45 @@ const DetaliiRelaxare = () => {
       <Navbar />
       <div className='container2'>
         <div className='text3'>
-          <h2 className='title'> Relaxează-te și Revitalizează-te cu Masajul de Relaxare!</h2>
+          <h1 className='title'> Relaxează-te și Revitalizează-te cu Masajul de Relaxare!</h1>
 
           <h2>Beneficiile Masajului de Relaxare</h2>
-          <p>1. Reducerea Stresului și Anxietății:
+          <ol>
+            <li>Reducerea Stresului și Anxietății:
+              </li>
             Masajul de relaxare induce o stare profundă de calm și relaxare, reducând nivelurile de cortizol (hormonul stresului) și ameliorând anxietatea.<br />
 
-            2. Ameliorarea Tensiunii Musculare:
+            <li> Ameliorarea Tensiunii Musculare: </li>
             Prin mișcări blânde și tehnici specifice, masajul ajută la eliberarea tensiunii acumulate în mușchi, oferindu-vă o senzație de ușurare și confort.<br />
 
-            3. Îmbunătățirea Circulației Sanguine:
+            <li> Îmbunătățirea Circulației Sanguine: </li>
             Masajul stimulează fluxul sanguin, ceea ce asigură un aport crescut de oxigen și nutrienți către celule, contribuind la regenerarea și revitalizarea țesuturilor.<br />
 
-            4. Îmbunătățirea Calității Somnului:
+            <li>  Îmbunătățirea Calității Somnului: </li>
             Relaxarea profundă obținută prin masaj vă ajută să aveți un somn mai odihnitor și mai profund, esențial pentru sănătatea fizică și mentală.<br />
 
-            5. Îmbunătățirea Stării de Spirit:
-            Masajul crește nivelurile de serotonină și dopamină, substanțe chimice din creier asociate cu starea de bine și fericirea.</p>
+            <li> Îmbunătățirea Stării de Spirit: </li>
+            Masajul crește nivelurile de serotonină și dopamină, substanțe chimice din creier asociate cu starea de bine și fericirea.</ol>
           <h2>Cum Funcționează Masajul de Relaxare</h2>
           <p>Masajul de relaxare folosește mișcări lente și blânde, precum frământarea, netezirea și mângâierea, pentru a relaxa mușchii și a calma mintea. Aceste tehnici sunt adaptate pentru a vă oferi o experiență plăcută și reconfortantă.</p>
 
-          <h3>Atmosferă Calmă:</h3>
+          <h2>Atmosferă Calmă:</h2>
           <p>O sesiune de masaj de relaxare se desfășoară într-un mediu liniștit și reconfortant, cu muzică relaxantă și arome plăcute, pentru a amplifica efectul terapeutic.</p>
           <h4>Recomandări pentru Eficiența Maximă a Tratamentului</h4>
           <p>Pentru a beneficia la maximum de masajul de relaxare, este recomandat să aveți ședințe regulate. De asemenea, completarea masajului cu tehnici de relaxare, cum ar fi yoga sau meditația, și adoptarea unui stil de viață sănătos vor contribui la menținerea stării de bine.</p>
-          <h3>Concluzie</h3>
+          <h2>Concluzie</h2>
           <p>Masajul de relaxare este soluția ideală pentru a vă deconecta de la stresul zilnic și pentru a vă revitaliza trupul și mintea. Beneficiile sale multiple, de la reducerea tensiunii musculare la îmbunătățirea calității somnului și a stării de spirit, fac din această tehnică o alegere excelentă pentru oricine dorește să își îmbunătățească starea generală de sănătate și de bine.</p>
-          <h3>Programează-te acum și descoperă puterea relaxării!</h3>
+          <h2>Programează-te acum și descoperă puterea relaxării!</h2>
           <p> Bucură-te de un moment de răsfăț și de liniște doar pentru tine!</p>
         </div>
 
-        <div>
-        <img src={relax} alt="image" className='image7' />
-        <img src={relax1} alt="image" className='image6' />
-        </div>
+        <table >
+          <tr>
+            <th> <img src={relax} alt="image" className='image7' /> </th>
+            <th> <img src={relax1} alt="image" className='image6' /> </th>
+            <th> <img src={scoica} alt="image" className='scoica' /> </th>
+        </tr>
+        </table>
       </div>
       <Form />
       <Footer />
